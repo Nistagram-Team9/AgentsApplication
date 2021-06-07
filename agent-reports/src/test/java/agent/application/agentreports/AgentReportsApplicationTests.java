4933365a810635833b5432ac433e02df93eba4c9
MarinaVojnovic <40699105+MarinaVojnovic@users.noreply.github.com>
2021-06-07 21:56:06 +0200

    #1-Initial project configuration for microservices - creating Spring … (#11)
    * #1-Initial project configuration for microservices - creating Spring boot applications and Dockerfiles, as well as docker compose.
    
    * #1 - Active profile set dinamically.

---
+++ agent-reports/src/test/java/agent/application/agentreports/AgentReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package agent.application.agentreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
